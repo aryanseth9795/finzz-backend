22541a4668cbe4bb29009901e0e084c59c7fda3c
aryanseth9795 <iiitianaryan@gmail.com>
2026-02-20 23:58:46 +0530

    frontend domain bug fix

--- server.ts
+++ server.ts
@@ -20,8 +20,6 @@ import errorMiddleware from "./src/middlewares/error.js";
 // DB
 import connectDb from "./src/db/db.js";
 
-
-
 // Connect to MongoDB
 
 connectDb(MongoURL);
@@ -31,9 +29,9 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["https://finzzadmin.netlify.app/", "http://localhost:5173"],
+    origin: ["https://finzzadmin.netlify.app", "http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
